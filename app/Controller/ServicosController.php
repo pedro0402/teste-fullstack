f4ee643e005ab86c32b2db0403b77a7a99ec05f5
Pedro Moraes <pedro693.ph@gmail.com>
2025-12-06 23:57:20 -0300

    refactor(servicos): refatorando index.ctp para adicionar styles e seguir design do projeto

--- app/Controller/ServicosController.php
+++ app/Controller/ServicosController.php
@@ -53,10 +53,7 @@ class ServicosController extends AppController
 		if ($this->request->is('post')) {
 			$this->Servico->create();
 			if ($this->Servico->save($this->request->data)) {
-				$this->Flash->success(__('The servico has been saved.'));
 				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Flash->error(__('The servico could not be saved. Please, try again.'));
 			}
 		}
 	}
@@ -75,10 +72,8 @@ class ServicosController extends AppController
 		}
 		if ($this->request->is(array('post', 'put'))) {
 			if ($this->Servico->save($this->request->data)) {
-				$this->Flash->success(__('The servico has been saved.'));
+	
 				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Flash->error(__('The servico could not be saved. Please, try again.'));
 			}
 		} else {
 			$options = array('conditions' => array('Servico.' . $this->Servico->primaryKey => $id));
@@ -99,11 +94,7 @@ class ServicosController extends AppController
 			throw new NotFoundException(__('Invalid servico'));
 		}
 		$this->request->allowMethod('post', 'delete');
-		if ($this->Servico->delete($id)) {
-			$this->Flash->success(__('The servico has been deleted.'));
-		} else {
-			$this->Flash->error(__('The servico could not be deleted. Please, try again.'));
-		}
+	
 		return $this->redirect(array('action' => 'index'));
 	}
 
